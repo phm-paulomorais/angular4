d410626061445cf8a68e7e8a97d9c04f463c7b33
Paulo Morais <phm.paulomorais@gmail.com>
2017-08-18 18:52:19 -0300

    Adicidos projetos do angular

---
+++ data-binding/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent,
+    InputPropertyComponent,
+    OutputPropertyComponent,
+    CicloComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MeuFormModule //,
+    //HttpModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
