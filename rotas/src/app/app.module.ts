9603416062417b67f9c80eda73d8eff79c976e4a
Paulo Morais <phm.paulomorais@gmail.com>
2017-08-21 18:01:45 -0300

    Implementacao até a aula 59

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterializeModule } from 'angular2-materialize';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+//import { CursosComponent } from './cursos/cursos.component';
+//import { routing } from './app.routing';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursosService } from './cursos/cursos.service';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosModule } from './cursos/cursos.module';
+import { AppRoutingModule } from './app.routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+//  CursosComponent,
+//  CursoDetalheComponent,
+//  CursoNaoEncontradoComponent
+  ],
+  imports: [
+    BrowserModule, 
+    FormsModule,
+    HttpModule,
+    MaterializeModule,
+    CursosModule,
+    AppRoutingModule
+//    routing
+  ],
+  providers: [/*CursosService*/],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
