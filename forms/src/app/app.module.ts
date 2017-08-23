1f07b37ff851f6a508441b88720c7b7e5ac593ae
Paulo Morais <phm.paulomorais@gmail.com>
2017-08-23 18:19:45 -0300

    adicionando projeto Forms...

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { DataFormComponent } from './data-form/data-form.component';
+//import { DataFormModule } from './data-form/data-form.module';
+import { TemplateFormModule } from './template-form/template-form.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AppRoutingModule,
+    TemplateFormModule//,
+    //DataFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+
+  usuario: any = {
+    nome: null,
+    email: null
+  }
+
+  onSubmit(form){
+    console.log(form);
+
+    //console.log(this.usuario);
+  }
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+  }
+
+  verificaValidTouched(campo){
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo){
+    return{
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultaCEP(cep, form){
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+     
+    //Verifica se campo cep possui valor informado.
+     if (cep != "") {
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
+
+          this.resetaDadosForm(form);
+
+          this.http.get(`//viacep.com.br/ws/${cep}/json`)
+              .map(dados => dados.json())
+              .subscribe(dados => this.populaDadosForm(dados, form));
+        }
+
+     }
+      
+
+  }
+
+  populaDadosForm(dados, formulario){
+    /*
+    formulario.setValue(
+      {
+          nome: formulario.value.nome,
+          email: formulario.value.nome.email,
+          endereco: {
+          rua: dados.logradouro,  
+          cep: dados.cep,
+          complemento: dados.complemento,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+          estado: dados.uf
+          } 
+      });
+      */
+
+      formulario.form.patchValue({
+        endereco: {
+          rua: dados.logradouro,  
+          //cep: dados.cep,
+          complemento: dados.complemento,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+          estado: dados.uf
+          }
+      });
+
+  }
+
+  resetaDadosForm(formulario){
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,  
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+        }
+    });
+  }
+
+}
